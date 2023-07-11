2c5a5a23244c482827265ef0a6163592395f3d1b
nourbenhazem <claudiabenhazem@gmail.com>
2023-07-11 15:06:45 +0100

    aa

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0 , 100):
-    if i == 99 :
-        print(" {:02d}" .format(i))
-    else :
-        print("{:02d} ,".format(i), end=" ")
+for num in range(0, 100):
+    if num == 99:
+        print("{0:02d}".format(num), end="\n")
+    else:
+        print("{0:02d}, ".format(num), end="")
