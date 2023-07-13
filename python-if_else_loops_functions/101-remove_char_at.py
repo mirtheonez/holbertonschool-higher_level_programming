b6197a23f4fc94c822a1e905fb7aec69e46f77c3
nourbenhazem <claudiabenhazem@gmail.com>
2023-07-13 11:28:05 +0100

    aa

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -4,11 +4,3 @@ def remove_char_at(str, n):
     if n < 0:
         return (str)
     return ( str[:n] + str[n+1:] )
-
-remove_char_at = __import__('101-remove_char_at').remove_char_at
-
-print(remove_char_at("Best School", 3))
-print(remove_char_at("Chicago", 2))
-print(remove_char_at("C is fun!", 0))
-print(remove_char_at("School", 10))
-print(remove_char_at("Python", -2))
