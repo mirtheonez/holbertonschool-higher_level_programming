238ad4ffcd70925be58b5138dbbb8571b50f02d2
nourbenhazem <claudiabenhazem@gmail.com>
2023-08-04 15:08:17 +0100

    square

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,28 +1,65 @@
 #!/usr/bin/python3
+""" Class Definition"""
+
+
 class Square:
-    """
-    Square class representation
-    Attributes:
-        __size (int): size of square
+    """CLass repres a square
+     Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, __size): Initializes a Square object.
     """
 
     def __init__(self, size=0):
-        self.size = size
+        """Initialize a Square object with the given size.
 
-    def area(self):
-        """square area"""
-        return self.__size**2
+        Args:
+            size (int, optional): The size of the square. Defaults to 0.
+
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+
+        Attributes:
+            __size (int): The size of the square.
+
+        """
+        self.size = size
 
     @property
     def size(self):
-        """size getter"""
-        return self.__size
+        """Get the size of the square.
+
+        Returns:
+            int: The size of the square.
+
+        """
+        return self._size
 
     @size.setter
-    def size(self, size):
-        """size setter"""
-        if type(size) != int:
+    def size(self, value):
+        """Set the size of the square.
+
+        Args:
+            value (int): The size of the square.
+
+        Raises:
+            TypeError: If value is not an integer.
+            ValueError: If value is less than 0.
+
+        """
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square.
+
+        Returns:
+            int: The area of the square.
+
+        """
+        return self._size**2
