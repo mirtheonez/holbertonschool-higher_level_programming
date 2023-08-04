b4166a745212a83882882917fa632d92f7438bf5
nourbenhazem <claudiabenhazem@gmail.com>
2023-08-04 15:06:03 +0100

    square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python3
+"""Class Definition"""
+
+
 class Square:
-    """
-    Square class representation
+    """A class repres a square.
+
     Attributes:
-        __size (int): size of square
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, __size): Initializes a Square object.
     """
 
     def __init__(self, size=0):
-        """ctor"""
+
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size
